784c8ec91180fb20981dd63f5768f3e9dfd2ce70
Vitalie Ciubotaru <vitalie@ciubotaru.tokyo>
2018-02-03 23:04:00 +0900

    Added 'board' command to AI

--- ai.c
+++ ai.c
@@ -34,6 +34,7 @@ void reset()
 void print_help(const char *topic)
 {
 	if (topic == NULL) {
+		dprintf(STDOUT_FILENO, "board height width - set game board size\n");
 		dprintf(STDOUT_FILENO, "load - load game from file\n");
 		dprintf(STDOUT_FILENO, "new - clear everything\n");
 		dprintf(STDOUT_FILENO, "ping - check if AI is alive\n");
@@ -42,12 +43,32 @@ void print_help(const char *topic)
 		dprintf(STDOUT_FILENO, "for details, type 'help [command]'\n");
 		return;
 	}
+	if (strcmp(topic, "board") == 0) {
+		dprintf(STDOUT_FILENO,
+			"board height width - set game board size\n");
+		return;
+	}
 	if (strcmp(topic, "new") == 0) {
 		dprintf(STDOUT_FILENO,
 			"new - clear everything and reset game environment\n");
 		return;
 	}
-	dprintf(STDOUT_FILENO, "%s: [to be added]\n", topic);
+	dprintf(STDOUT_FILENO, "%s: no help for this topic\n", topic);
+}
+
+int set_grid(const uint16_t height, const uint16_t width) {
+	if (world == NULL) create_world();
+	if (world->grid != NULL) {
+		dprintf(STDOUT_FILENO, "Error: bad command (board already set, use 'new')\n");
+		return 1;
+	}
+	grid_t *grid = create_grid(height, width);
+	if (grid == NULL) {
+		dprintf(STDOUT_FILENO, "Error: internal error (failed to create grid)\n");
+		return 1;
+	}
+	dprintf(STDOUT_FILENO, "ack\n");
+	return 0;
 }
 
 void standby()
@@ -65,6 +86,27 @@ void standby()
 		char *command = strdup(stdin_buffer->string);
 		stdin_buffer->size = 0;
 		char *token = strtok(command, " \n");	/* remove trailing newline */
+		if (!strcmp(token, "board")) {
+			uint16_t coords[2];
+			int i;
+			int success = 1;
+			for (i = 0; i < 2; i++) {
+				token = strtok(NULL, " \n");
+				if (!token) {
+					print_help("board");
+					success = 0;
+					break;
+				}
+				coords[i] = (uint16_t) atoi(token);
+				if (coords[i] == 0) {
+					dprintf(STDOUT_FILENO, "Error: bad argument\n");
+					success = 0;
+					break;
+				}
+			}
+			if (success) set_grid(coords[0], coords[1]);
+			continue;
+		}
 		if (!strcmp(token, "help")) {
 			token = strtok(NULL, " \n");
 			print_help(token);
